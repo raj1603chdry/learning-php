16dfadc26397bc9aa92ff6ba4ca3584c5ef10eb6
Raj Choudhary <raj.choudhary1603@gmail.com>
2017-06-17 12:53:00 +0530

    added comments

--- array.php
+++ array.php
@@ -1,5 +1,6 @@
 <?php
-	$food = array('pizza','pasta','bread');	//	here array() is a function, which is used to create an array of comma-separated values within it.
+	$food = array('pizza','pasta','bread');
+	//	here array() is a function, which is used to create an array of comma-separated values within it.
 
 	echo $food[1];
 	echo '<br>';
@@ -11,7 +12,8 @@
 	print_r($food);
 	echo '<br>';
 
-	$food1 = array('pizza'=>200, 'pasta'=>250, 'salad'=>50);	//	here we have created something like a map of java (or c++) or set of python which is of the form key=>value. Now instead of the traditional index, the new key is used to find the associated value. This tyoe of array is known as an associative array.
+	$food1 = array('pizza'=>200, 'pasta'=>250, 'salad'=>50);
+	//	here we have created something like a map of java (or c++) or set of python which is of the form key=>value. Now instead of the traditional index, the new key is used to find the associated value. This tyoe of array is known as an associative array.
 
 	print_r($food1);
 	echo '<br>';
